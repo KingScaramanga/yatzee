6af5734ee1a28a288f9638ac53d9aca55cdbd45b
KingScaramanga <alexandre.ziaja@gmail.com>
2024-05-16 23:19:58 +0200

    - clean code

--- src/main/java/org/codingdojo/yatzy1/categoryManagement/category/TwoPairsCategory.java
+++ src/main/java/org/codingdojo/yatzy1/categoryManagement/category/TwoPairsCategory.java
@@ -2,11 +2,11 @@ package org.codingdojo.yatzy1.categoryManagement.category;
 
 import org.codingdojo.yatzy1.categoryManagement.Category;
 import org.codingdojo.yatzy1.diceManagement.Dice;
-
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class TwoPairsCategory implements Category {
+
     @Override
     public int calculate(Dice[] diceFromRoll) {
         Map<Dice, Integer> occurenceDiceMap = CategoryUtil.getOccurenceDiceMap(diceFromRoll);
@@ -14,7 +14,7 @@ public class TwoPairsCategory implements Category {
             .stream()
             .filter(dice->occurenceDiceMap.get(dice)>1)
             .toList();
-
+        //TODO : deal with complicated pairs like 2 pairs from fourth of a kind
         if (dicePairs.size()>1){
             return dicePairs.stream().mapToInt(dice->dice.getValue()*2).sum();
         } else {
