f99b0f74263c2404731314daaf4d76c6dbfc28eb
KingScaramanga <alexandre.ziaja@gmail.com>
2024-05-18 14:54:17 +0200

    - modify package organization - cleaning code

--- src/main/java/org/codingdojo/yatzy1/categoryManagement/common/interfaces/Category.java
+++ src/main/java/org/codingdojo/yatzy1/categoryManagement/common/interfaces/Category.java
@@ -2,7 +2,7 @@ package org.codingdojo.yatzy1.categoryManagement.common.interfaces;
 
 import org.codingdojo.yatzy1.diceManagement.Dice;
 
-import java.util.List;
+
 
 public interface Category {
     int calculate(Dice[] dices);
