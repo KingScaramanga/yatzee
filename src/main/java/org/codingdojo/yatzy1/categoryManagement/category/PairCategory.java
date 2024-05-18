9d84b4be78c2f9e9d936a76d238eacb8468e157e
KingScaramanga <alexandre.ziaja@gmail.com>
2024-05-18 10:54:29 +0200

    - implement Three of a kind category - finalize two pairs category

--- src/main/java/org/codingdojo/yatzy1/categoryManagement/category/PairCategory.java
+++ src/main/java/org/codingdojo/yatzy1/categoryManagement/category/PairCategory.java
@@ -3,9 +3,7 @@ package org.codingdojo.yatzy1.categoryManagement.category;
 import org.codingdojo.yatzy1.categoryManagement.Category;
 import org.codingdojo.yatzy1.diceManagement.Dice;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Map;
 
 public class PairCategory implements Category {
